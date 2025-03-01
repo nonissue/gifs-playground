e9f4366d6d915da9f2fc18c3917703513bf473fa
nonissue <andy@nonissue.org>
2025-02-28 21:57:45 -0700

    is working, super slow, need a better solution as right now i cant filter both timestamp and day

--- src/getUpcomingDepartures.ts
+++ src/getUpcomingDepartures.ts
@@ -1,29 +1,66 @@
 const readCsv = require('gtfs-utils/read-csv');
 const computeStopovers = require('gtfs-utils/compute-stopovers');
+import { isAfterNow } from './lib/isAfterNow';
+import { isOnDay } from './lib/isOnDay';
 // Accepts a stop id as a param, computes N next upcoming stopovers at X stop
 
-const getUpcomingDepartures = async (stop_id: string, limit: number = 5) => {
-    if (!stop_id) {
-        throw new Error('stop_id is required');
+export const getUpcomingDepartures = async (stopId: string, limit: number = 5) => {
+    if (!stopId) {
+        throw new Error('stopId is required');
     }
 
-    const readFile = (file: any) => {
-        return readCsv(require.resolve('../data/gtfs_sorted/' + file + '.txt'));
+    const readFile = (file: string) => {
+        return readCsv(require.resolve(`../data/gtfs_sorted/${file}.txt`));
     };
 
-    // process.exit(1);
-    const stopovers = computeStopovers(readFile, 'America/Edmonton', {
-        stopTime: (s: any) => {
-            if (s.stop_id === stop_id) {
-                // console.log(s);
-                console.log(Object.keys(s));
-                console.log(s.departure_time);
-                return s;
-            }
-        },
-    });
+    const stopovers = computeStopovers(readFile, 'America/Edmonton'); // Adjusted to America/Edmonton
+    const upcomingStopovers: any[] = [];
+    // let counter = 0;
 
     for await (const stopover of stopovers) {
-        console.log(stopover);
+        // counter = counter + 1;
+        // console.log(counter);
+        if (stopover.stop_id !== stopId) {
+            continue; // Filter by stop_id
+        }
+
+        // console.log('stopId match! checking day...');
+
+        // if (isOnDay(stopover.arrival, '2025-02-28')) {
+        //     console.log('date is today!');
+        // } else {
+        //     continue;
+        // }
+
+        // console.log('stopId match!');
+
+        // Check if the stopover is after the current time
+
+        // if (isAfterNow(stopover.arrival) && isOnDay(stopover.start_of_trip, '2025-02-28')) {
+        // if (isOnDay(stopover.arrival, '2025-02-28')) {
+        if (isAfterNow(stopover.arrival)) {
+            // console.log('TODAY AND is after current time!');
+            upcomingStopovers.push(stopover);
+        }
+
+        // if (isAfterNow(stopover.arrival)) {
+        //     console.log('is after current time!');
+        //     upcomingStopovers.push(stopover);
+        // }
+
+        if (upcomingStopovers.length >= limit) {
+            break;
+        }
+        // Stop collecting once the limit is reached
     }
+
+    // Sort the upcoming stopovers by arrival time (ascending)
+    // upcomingStopovers.sort((a, b) => a.arrival - b.arrival);
+
+    return upcomingStopovers;
 };
+
+(async () => {
+    const res = await getUpcomingDepartures('2114', 5);
+    console.log(res);
+})();
