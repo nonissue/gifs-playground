07e4f044b37068e2ff6449dba65b8a2d0b182370
nonissue <andy@nonissue.org>
2025-02-28 21:56:34 -0700

    simply isAfterNow bigtime

--- src/lib/isAfterNow.ts
+++ src/lib/isAfterNow.ts
@@ -1,7 +1,7 @@
 const { DateTime } = require('luxon');
 
 export const isAfterNow = (arrivalTimestamp: number): boolean => {
-    const now = DateTime.now().setZone('America/Edmonton');
-    const arrivalTime = DateTime.fromMillis(arrivalTimestamp * 1000, { zone: 'America/Edmonton' });
-    return arrivalTime > now; // Check
+    const now = DateTime.now().setZone('Europe/Berlin').toMillis() / 1000;
+
+    return arrivalTimestamp > now; // Check
 };
