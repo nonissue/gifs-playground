f518adbbb43bb5eaa714e6b95d4b642f92c88c06
nonissue <andy@nonissue.org>
2025-02-28 20:00:14 -0700

    buno

--- src/getUpcomingDepartures.ts
+++ src/getUpcomingDepartures.ts
@@ -1,3 +1,27 @@
 // Accepts a stop id as a param, computes N next upcoming stopovers at X stop
 
-const getUpcomingDepartures = (stop_id: string, limit = 5) => {};
+const getUpcomingDepartures = async (stop_id: string, limit: number = 5) => {
+    if (!stop_id) {
+        throw new Error('stop_id is required');
+    }
+
+    const readFile = (file: any) => {
+        return readCsv(require.resolve('../data/gtfs_sorted/' + file + '.txt'));
+    };
+
+    // process.exit(1);
+    const stopovers = computeStopovers(readFile, 'America/Edmonton', {
+        stopTime: (s: any) => {
+            if (s.stop_id === stop_id) {
+                // console.log(s);
+                console.log(Object.keys(s));
+                console.log(s.departure_time);
+                return s;
+            }
+        },
+    });
+
+    for await (const stopover of stopovers) {
+        console.log(stopover);
+    }
+};
