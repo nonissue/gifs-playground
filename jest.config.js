d1680500dbbd38d87119780335cb7b222fc15b0b
nonissue <andy@nonissue.org>
2025-02-28 21:45:44 -0700

    jest should ignore js files and node_modules

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
-  },
+    testEnvironment: 'node',
+    transform: {
+        '^.+\.tsx?$': ['ts-jest', {}],
+    },
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
